d90892a91c756619222095a197e72d9c430b9f48
Simono il magnifico <simoneromano@tutanota.de>
2021-01-21 19:19:42 +0100

    add subscriptions to schema

--- src/schema.ts
+++ src/schema.ts
@@ -2,12 +2,12 @@ import { makeSchema } from "nexus"
 import { join } from "path"
 
 import { AuthMutation, AuthQuery } from "./graphql/auth"
-import { OrderMutation, OrderQuery } from "./graphql/order"
+import { OrderMutation, OrderQuery, OrderSubscription } from "./graphql/order"
 
 import { GQLDateTime, Upload } from "./typings"
 
 export const schema = makeSchema({
-  types: [GQLDateTime, Upload, AuthQuery, AuthMutation, OrderQuery, OrderMutation],
+  types: [GQLDateTime, Upload, AuthQuery, AuthMutation, OrderQuery, OrderMutation, OrderSubscription],
   outputs: {
     typegen: join(__dirname, "generated", "typegen.ts"),
     schema: join(__dirname, "generated", "schema.graphql"),
